8ecde385258cd644683ca84c3ebc5bf29f1aa7c1
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2026-07-12 20:19:22 +0800

    remove static parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/uart/SimpleUartAgreement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/uart/SimpleUartAgreement.java
@@ -13,8 +13,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SimpleUartAgreement {
-    protected static String[] possibleReceivingMessageTypeAndCode = {"rDAT"};
-    protected static int generalValueLength = 4;
+    protected String[] possibleReceivingMessageTypeAndCode = {"rDAT"};
+    protected int generalValueLength = 4;
     UsbUart usbUart;
     private final ConcurrentLinkedQueue<String> receivedMessages = new ConcurrentLinkedQueue<>();
     private final ConcurrentHashMap<String, CopyOnWriteArrayList<String>> receivedValues = new ConcurrentHashMap<>();
